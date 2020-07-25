8d847e4f83979a6a7f6e473afb43d6e1a021f9b8
Bent-Project <bentdputra@gmail.com>
2020-07-25 22:19:16 +0700

    make a Card html

--- bent-project/src/App.js
+++ bent-project/src/App.js
@@ -1,25 +1,69 @@
 import React from 'react';
 import logo from './logo.svg';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonItem, IonIcon, IonLabel, IonButton } from '@ionic/react';
+import { pin, wifi, wine, warning, walk } from 'ionicons/icons';
 import './App.css';
+
 
 function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React {props.semangat}
-        </a>
-      </header>
-    </div>
+    <>
+     <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Bent Project</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle>RESUME</IonCardSubtitle>
+            <IonCardTitle>About Me</IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent>
+            Saya biasa dipanggil BENT, yang sebenarnya nama saya Bentang Wijayandaru Putra
+            saya tahun ini berumur 28 tahun, sudah menikah, serta sudah mempunyai 1 orang anak.
+      </IonCardContent>
+        </IonCard>
+
+        <IonCard>
+          <IonItem>
+            <IonIcon icon={pin} slot="start" />
+            <IonLabel>ion-item in a card, icon left, button right</IonLabel>
+            <IonButton fill="outline" slot="end">View</IonButton>
+          </IonItem>
+
+          <IonCardContent>
+            Untuk lebih lanjut mengenai saya,
+            Anda bisa meng klik tombol - tombol dibawah ini.
+      </IonCardContent>
+        </IonCard>
+
+        <IonCard>
+          <IonItem href="#" className="ion-activated">
+            <IonIcon icon={wifi} slot="start" />
+            <IonLabel>Card Link Item 1 activated</IonLabel>
+          </IonItem>
+
+          <IonItem href="#">
+            <IonIcon icon={wine} slot="start" />
+            <IonLabel>Card Link Item 2</IonLabel>
+          </IonItem>
+
+          <IonItem className="ion-activated">
+            <IonIcon icon={warning} slot="start" />
+            <IonLabel>Card Button Item 1 activated</IonLabel>
+          </IonItem>
+
+          <IonItem>
+            <IonIcon icon={walk} slot="start" />
+            <IonLabel>Card Button Item 2</IonLabel>
+          </IonItem>
+        </IonCard>
+      </IonContent>
+    </IonPage>
+    </>
   );
 }
 
